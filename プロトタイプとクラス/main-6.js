1349ec5856b0b0cc1a885b287a84b969b3145358
minori <pyon02.hbk@gmail.com>
2025-08-10 04:23:39 +0900

    js: Prototypeとclassの復習・補足

--- プロトタイプとクラス/main-6.js
+++ プロトタイプとクラス/main-6.js
@@ -85,11 +85,8 @@ for (const key of Object.keys(obj2)) {
 }
 
 /*
-クラス
-似たようなオブジェクトをたくさん作る設計図
+ファクトリー関数
 */
-
-// 例えばuserをたくさん作りたいとき
 const user1 = {
   name: 'minori',
   age: 28,
@@ -108,8 +105,7 @@ const user3 = {
   greeting() {},
 };
 
-// 上記を関数にまとめるとシンプルにできる = factory関数
-// パスカルケース = 似たようなオブジェクトを作成したいんだなとわかる
+// 変数名はパスカルケースにする
 let UserFactory = (name, age) => {
   return {
     name,
@@ -118,11 +114,15 @@ let UserFactory = (name, age) => {
   };
 };
 
+// user4などは「インスタンス」という
 const user4 = UserFactory('sakura', 28);
 const user5 = UserFactory('ino', 29);
 const user6 = UserFactory('rock', 30);
 
-// 上記の専用の関数がある = コンストラクタ関数（アロー関数はだめ）
+/*
+コンストラクタ関数
+アロー関数はだめ
+*/
 // 呼び出すときに new演算子 をつける
 const UserConstructor = function (name, age) {
   // this{} // newをつけることで内部的に生成される
@@ -139,13 +139,14 @@ const user9 = new UserConstructor('temari', 28);
 console.log(user7);
 
 /*
-共通しているメソッドの関数オブジェクトを簡潔にする場合 上記は同様の関数を各objに共有しておりメモリを余分に占めてしまう
+共通しているメソッドの関数オブジェクトを簡潔にする場合
+上記のファクトリー関数、コンストラクタ関数は同様の関数を各objに共有しておりメモリを余分に占めてしまう
 */
 // 方法1 関数を別で定義して、オブジェクトに入れる
 const greeting = function () {
   return `Hi! This is ${this.name}. I am ${this.age} years old.`;
 };
-// 方法2 プロトタイプに入れる
+// 方法2 ファクトリー関数でプロトタイプに入れる場合
 const userPrototype = {
   greeting() {
     return `Hi! This is ${this.name}. I am ${this.age} years old.`;
@@ -158,16 +159,13 @@ let UserFactory2 = (name, age) => {
   return user;
 };
 
-const user10 = UserFactory2('hanako', 28);
+const user10 = UserFactory2('hanako', 28); // prototypeの中にgreetingが入っている
 const user11 = UserFactory2('tarou', 29);
 
-console.log(user10); // prototypeの中にgreetingが入っている
-console.log(user11);
-
-// 方法3 コンストラクタ関数の場合
+// 方法3 コンストラクタ関数でのプロトタイプに入れる場合
 const UserConstructor2 = function (name, age) {
   // this{} // 厳密には下記が生成される
-  // this = Object.create(userPrototype); // thisの初期値
+  // this = Object.create(UserConstructor2.prototype); // thisの初期値
   this.name = name;
   this.age = age;
   // this.greeting = greeting; 不要
@@ -181,57 +179,16 @@ UserConstructor2.prototype.greeting = function () {
 
 const user12 = new UserConstructor2('maru', 28);
 const user13 = new UserConstructor2('sankaku', 29);
-console.log(user12);
-console.log(user13);
 
 console.log(UserConstructor2.prototype === user12.__proto__); // true
 console.log(user12.__proto__ === user13.__proto__); // true
 
 /*
-内蔵されているコンストラクタ関数
-*/
-// new Object() オブジェクトを作るコンストラクタ関数 ( o = {}; と同じ)
-let o = new Object({ hi: 'hi' });
-Object.prototype.hello = 'Hello'; // prototypeの中にhelloが入る
-o = {}; // ここにもhelloが入る
-console.log(o);
-
-// 配列
-const a = new Array('Apple', 'Banana');
-console.log(a);
-
-// 関数
-const f = new Function('a', 'b', 'return a + b');
-console.log(f(3, 4));
-
-/*
-hasOwnProperty
-*/
-o = {
-  a: 1,
-};
-console.log(o);
-// aが存在するか確認
-console.log(o.hasOwnProperty('a')); // true
-// in演算子との違い = in演算子はプロトタイプチェーンまで全部見る
-console.log(o.hasOwnProperty('hello')); // false
-console.log('hello' in o); // true
-
-// プロトタイプのメソッドはthisを使用しているため、下記の記述ではオブジェクトを指定していない
-console.log(Object.prototype.hasOwnProperty('a')); // false
-// 上記と全く同じ
-console.log(Object.hasOwn('a')); // false
-// callでオブジェクトを使用できる
-console.log(Object.prototype.hasOwnProperty.call(o, 'a')); // true
-
-/*
 new.target
-// コンストラクタ関数内で使用するとコンストラクタ関数を返す
-// newオブジェクトのtargetプロパティという意味ではない = new.targetはセット
-// 普通の関数で使用するとundefined
+コンストラクタ関数内で使用するとコンストラクタ関数を返す
+普通の関数内で使用するとundefined
 */
 const UserConstructor3 = function (name, age) {
-  // this = Object.create(userPrototype);
   console.log(new.target);
   this.name = name;
   this.age = age;
@@ -240,18 +197,10 @@ const UserConstructor3 = function (name, age) {
   // return this
 };
 
-UserConstructor3.prototype.greeting = function () {
-  return `Hi! This is ${this.name}. I am ${this.age} years old.`;
-};
-
-const user14 = new UserConstructor3('circle', 28);
-const user15 = new UserConstructor3('shikaku', 29);
-console.log(user14);
-console.log(user15);
-
 /*
 class構文
-// コンストラクタ関数の上位互換
+コンストラクタ関数の上位互換
+class内はデフォルトでstrictモード
 */
 class User {
   // constructor = コンストラクター関数でいう、メインの処理と同じになる
@@ -259,16 +208,14 @@ class User {
     this.name = name;
     this.age = age;
   }
-  // {}の中身には、省略記法のメソッドしか書けない それ以外はエラー
-  // メソッドを羅列することで、prototype内に関数を入れることができる
+  // prototype内に関数を入れることができる
+  // 省略記法のメソッドしか書けない それ以外はエラー
   greeting() {}
   post() {}
 }
-console.dir(User); // 関数オブジェクト（内部的には区別されている）
 
 // 呼び出す時は new をつけて呼び出す
-const user = new User();
-console.dir(user1);
+const user = new User('minori', 28);
 
 /*
 class構文
@@ -281,6 +228,8 @@ class User2 {
   }
   get greeting() {} // get つけるだけ
   set post(value) {} // set つけるだけ
+  // 内部的に下記のような処理を簡単にできる
+  // Object.defineProperty(変数名.prototype,''){ get(),set()}
 }
 console.dir(User2); // prototype内にget greetingとset postが追加されている
 
@@ -294,10 +243,11 @@ class User3 {
     this.age = age;
   }
   static greeting() {} // メソッドの前にstaticを記載
+  // 下記と同じような処理
+  // User.greeting = function () {}
   static get greeting() {} // getterにも使える
   set post(value) {}
 }
-console.dir(User3);
 
 /*
 フィールド
@@ -399,3 +349,40 @@ const rabbitObj = {
   },
 };
 rabbitObj.eat();
+
+/*
+内蔵されているコンストラクタ関数
+*/
+// new Object() オブジェクトを作るコンストラクタ関数 ( o = {}; と同じ)
+let o = new Object({ hi: 'hi' });
+Object.prototype.hello = 'Hello'; // prototypeの中にhelloが入る
+o = {}; // ここにもhelloが入る
+console.log(o);
+
+// 配列
+const a = new Array('Apple', 'Banana');
+console.log(a);
+
+// 関数
+const f = new Function('a', 'b', 'return a + b');
+console.log(f(3, 4));
+
+/*
+hasOwnProperty
+*/
+o = {
+  a: 1,
+};
+console.log(o);
+// aが存在するか確認
+console.log(o.hasOwnProperty('a')); // true
+// in演算子との違い = in演算子はプロトタイプチェーンまで全部見る
+console.log(o.hasOwnProperty('hello')); // false
+console.log('hello' in o); // true
+
+// プロトタイプのメソッドはthisを使用しているため、下記の記述ではオブジェクトを指定していない
+console.log(Object.prototype.hasOwnProperty('a')); // false
+// 上記と全く同じ
+console.log(Object.hasOwn('a')); // false
+// callでオブジェクトを使用できる
+console.log(Object.prototype.hasOwnProperty.call(o, 'a')); // true
