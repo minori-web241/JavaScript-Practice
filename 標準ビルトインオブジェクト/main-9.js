a6bc15626a77e1611e2b989509bcb85353bc85b9
minori <pyon02.hbk@gmail.com>
2025-08-12 00:17:58 +0900

    js: 正規表現

--- 標準ビルトインオブジェクト/main-9.js
+++ 標準ビルトインオブジェクト/main-9.js
@@ -179,3 +179,133 @@ result = apple.indexOf('l', -3); // 10
 
 // 見つからない場合は -1
 result = apple.indexOf('banana'); // -1
+
+result = apple.includes('banana'); // false
+
+/*
+正規表現 - regexp.test
+*/
+let regexp = new RegExp('appels');
+// リテラル表記で正規表現を再代入（"appels" という文字列にマッチするパターン）
+regexp = /appels/;
+// 大文字小文字を区別せずに判定したい場合
+regexp = /appels/i;
+
+// test() は「対象文字列がパターンにマッチするか」を true / false で返す
+result = regexp.test('I like appels');
+
+// 数字が入っていたらtrueを返したい 0-9の数字を一つ取れる
+regexp = /appels\d/;
+result = regexp.test('I like appels5');
+
+// スペースならtrue
+regexp = /\s/;
+// 全角もtrue
+result = regexp.test(' '); // true
+result = regexp.test('\n'); // true
+
+// 数字もアルファベットもアンダースコアもOk
+regexp = /\w/;
+result = regexp.test(' h '); // false
+result = regexp.test('_'); // true
+
+// 数字以外のもの
+regexp = /\D/;
+
+// スペース以外
+regexp = /\S/;
+
+// ワード以外
+regexp = /\W/;
+
+// 改行以外全て
+regexp = /./;
+
+// エスケープ
+regexp = /\./;
+result = regexp.test('.'); // true
+
+// キャレット
+// 先頭にキャレットで指定したものがあれば
+regexp = /^apple/;
+result = regexp.test('apple is red'); // true
+
+// ダラー
+// キャレットの逆
+regexp = /apple$/;
+result = regexp.test('apple is red'); // false
+
+// appleのみ
+regexp = /^apple$/;
+result = regexp.test('apple'); // true
+result = regexp.test('apples'); // false
+
+// 改行
+regexp = /^apple$/m;
+result = regexp.test('I like \napple'); // true
+
+// 連続で表示
+regexp = /\ab{5}/;
+result = regexp.test('bbabbbbbb'); // true
+regexp = /a+/; // /a{1,}/; と同じ
+regexp = /a?/; // /a{0,1}/; と同じ
+regexp = /a*/; // /a{0,}/; と同じ
+regexp = /(hey)+/; // 全体を繰り返し // heyheyheyhey
+
+// 単語ずつとる
+regexp = /I like (apple|banana|orange)/;
+result = regexp.test('I like banana'); // true
+
+// []内の一文字だけ 内部にエスケープ処理不要
+regexp = /b[ua]g/;
+// -で範囲を指定することができる
+regexp = /b[a-z]g/;
+
+// 応用 - メールアドレス
+regexp = /[-.\w]+@([-\w]+\.)+[-\w]+/;
+result = regexp.test('test@test.com'); // true
+
+/*
+正規表現 - メソッド
+*/
+// gフラッグ
+// どのポジションから調べるか決めることができる
+regexp = /[-.\w]+@([-\w]+\.)+[-\w]+/g;
+
+// lastIndex
+// regexp.lastIndex = 5; // 文字列の5番目から調べる デフォルトは0
+
+// 複数の文字列がある場合
+let mail = 'test@test.com, example@example.com';
+result = regexp.test(mail); // 繰り返す lastIndexが更新される
+
+// exec
+result = regexp.exec(mail); // 繰り返す lastIndexが更新される
+
+// search indexOfに似ている
+result = mail.search('@'); // 4
+
+// match
+// 該当したものを配列にして返してくれる よく使われる
+result = mail.match(regexp);
+
+// matchAll
+// gフラッグが必須
+// iterable= 反復処理できるオブジェクト
+result = mail.matchAll(regexp);
+for (const item of result) {
+  console.log(item);
+}
+
+// replace
+result = mail.replace(/@/g, '*'); // gフラッグで全て
+
+// replaceAll 正規表現を使用する必要がない
+result = mail.replaceAll('@', '*');
+
+// split
+// 配列に変える
+result = mail.split('@', '*');
+
+console.dir(result);
+console.dir(regexp);
